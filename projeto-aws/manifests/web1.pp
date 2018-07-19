6869254a392f721c2f2ec7eebecdf7edc8c9175c
root <root@centosserver.dominiolinux.net>
2018-07-19 16:00:43 -0300

    AWS

---
+++ projeto-aws/manifests/web1.pp
@@ -0,0 +1,19 @@
+exec {'apt-get update':
+	command => "/usr/bin/apt-get update",
+}
+
+package { ["vim", "curl", "git"]:
+	ensure	=> installed,
+	require => Exec["apt-get update"],
+}
+
+class { "nginxphp":
+	doc_root => "/vagrant/web1",
+	server_name => $ipaddress_eth0,
+	require => Exec["apt-get update"],
+
+}
+
+notice("Seu ip é $ipaddress_eth0")
+	
+
